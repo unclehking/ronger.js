39ce59719076ac84a7846b6a24629f29aa0442b5
unclehking <513935463@qq.com>
2016-11-30 10:00:46 +0800

    new

--- ronger.js
+++ ronger.js
@@ -2,8 +2,7 @@
 
 
 ;(function(window,document) {
-    var w = window,
-        doc = document;
+    var w = window,doc = document;
     var ronger = function(selector) {
         return new ronger.prototype.init(selector);
     }
@@ -12,7 +11,7 @@
         length : 0,
         splice: [].splice,
         selector : '',
-        init : function(selector) {//dom选择的一些判断
+        init : function(selector) {
 			if(!selector) { return this; }
 			var selector = selector.trim(),
 				elm;
@@ -40,15 +39,27 @@
 					return false;
 			}
 			return this;
-		}
+		},
+        hide:function(){
+            Array.from(this).forEach(function(e,i){
+                e.style.display = "none";
+            });
+        },
+        show:function(){
+            Array.from(this).forEach(function(e,i){
+                e.style.display = "";
+            });
+        },
+        remove:function(){
+            Array.from(this).forEach(function(el,i){
+                el.parentNode.removeChild(el);
+            });
+        }
     }
     ronger.prototype.init.prototype = ronger.prototype;
-
-    ronger.ajax = function(){ 
+    ronger.ajax = function(){
         console.log(this);
     };
-
-
 
     window.jR = ronger;
 })(window,document);
